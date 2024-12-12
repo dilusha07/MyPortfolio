086c6a523f5f4e3da59fed8c029db03a50d6ead0
dilusha07 <dilushasandaruwani07@gmail.com>
2024-12-12 22:45:58 +0530

    update UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import Banner from "./components/banner/Banner";
+import Navbar from "./components/navbar/Navbar";
+import Features from "./components/features/Features";
+import Projects from "./components/projects/Projects";
+import Resume from "./components/resume/Resume";
+import Testimonial from "./components/tesimonial/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import FooterBottom from "./components/footer/FooterBottom";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Helloo</h1>
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+       <Navbar/>
+      <div className="max-w-screen-xl mx-auto">
+        <Banner/>
+        <Features/>
+        <Projects />
+        <Resume />
+        <Testimonial />
+        <Contact />
+        <Footer />
+        <FooterBottom/>
+        
+      </div>
     </div>
   );
 }
